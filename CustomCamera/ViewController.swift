2b3106c7ad3eb89b12b1cdca30b372e3e23deb94
Developer Shanghai <iamasimkhanjadoon@gmail.com>
2020-05-08 13:20:12 +0400

    Initial Commit

---
+++ CustomCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCamera
+//
+//  Created by Dev Shanghai on 08/05/2020.
+//  Copyright © 2020 Dev Shanghai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
